1d6c26caedd4c434caf7a827cfc576124aa71b75
Quan Trinh <110477131+qtrinh395@users.noreply.github.com>
2023-07-25 16:51:51 -0400

    test

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -1,8 +1,10 @@
 VENV_PATH=$1
 
 if [ -e "$VENV_PATH" ]; then
+    echo "Env Path Found: $VENV_PATH"
     echo "Activating venv"
     source $VENV_PATH/bin/activate
+    pip freeze
     echo "Running tests..."
     pytest ./tests
 else
