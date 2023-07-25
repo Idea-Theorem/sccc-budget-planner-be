f8eafedb1f7a4b008c8e17346aeff7966a39d42b
Quan Trinh <110477131+qtrinh395@users.noreply.github.com>
2023-07-25 13:50:32 -0400

    test

--- CI/setup.sh
+++ CI/setup.sh
@@ -23,7 +23,7 @@ fi
 # Create a virtual environment and activate it
 if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv venv
+    python3 -m venv $APPENV_PATH
 fi
 
 echo "Activating virtual environment..."
