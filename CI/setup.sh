4717be936430899fb47baa667d0e65f7346d7ad1
Quan Trinh <110477131+qtrinh395@users.noreply.github.com>
2023-07-25 14:38:30 -0400

    change path

--- CI/setup.sh
+++ CI/setup.sh
@@ -38,7 +38,7 @@ fi
 # Install Python app requirements using pip
 if [ -f "requirements.txt" ]; then
     echo "Installing app requirements..."
-    pip install  -r requirements.txt --upgrade
+    pip install --target='./venv/lib/python3.8/site-packages' -r requirements.txt --upgrade
 else
     echo "Requirements file 'requirements.txt' not found. Please make sure it exists in the current directory."
 fi
