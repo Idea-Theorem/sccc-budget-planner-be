68d8229ccc2613baca62be3e6d9f742e85a74ffb
dev-minified <frontendrock@gmail.com>
2024-03-28 14:46:28 +0500

    updated validation for program schema

--- src/validators/program.ts
+++ src/validators/program.ts
@@ -7,9 +7,27 @@ export const programSchema = {
         department_id: Joi.string().required(),
         from_date: Joi.date().iso().required(),
         to_date: Joi.date().iso().required(),
-        income: Joi.array().items(Joi.object().unknown()).required(),
-        supply_expense: Joi.array().items(Joi.object().unknown()).required(),
-        salary_expense: Joi.array().items(Joi.object().unknown()).required(),
+        income: Joi.array().items(
+            Joi.object({
+                id: Joi.string().required(),
+                name: Joi.string().required(),
+                amount: Joi.number().required()
+            })
+        ).required(),
+        supply_expense: Joi.array().items(
+            Joi.object({
+                id: Joi.string().required(),
+                name: Joi.string().required(),
+                amount: Joi.number().required()
+            })
+        ).required(),
+        salary_expense: Joi.array().items(
+            Joi.object({
+                id: Joi.string().required(),
+                name: Joi.string().required(),
+                amount: Joi.number().required()
+            })
+        ).required(),
         status: Joi.string().valid('PENDING', 'REJECTED', 'APPROVED', 'DRAFTED').required(),
         created_at: Joi.date().default('now'),
         updated_at: Joi.date().allow(null).optional()
