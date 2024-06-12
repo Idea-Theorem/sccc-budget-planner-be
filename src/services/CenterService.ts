9131260a87b1115acd79bb4c7d209efb3c25cdb8
dev-minified <frontendrock@gmail.com>
2024-06-12 18:56:18 +0500

    new calculation module

--- src/services/CenterService.ts
+++ src/services/CenterService.ts
@@ -40,12 +40,46 @@ export default {
     },
 
     getDepartmentById: async (centerId: string) => {
-        const departments = await prisma.department.findMany({
+        const departments: any = await prisma.department.findMany({
             where: {
                 center_id: centerId
             }
         });
-        return departments;
+        const department = await fetchProgramsAndCalculateAmounts(departments)
+        const totalDepartmentBudget = departments.reduce((sum: any, department: any) => sum + department.totalAmount, 0);
+
+        return { department, totalDepartmentBudget };
+
+        async function fetchProgramsAndCalculateAmounts(departmentArray: any) {
+            for (const department of departmentArray) {
+                // Fetch programs associated with the department
+                const programs = await prisma.program.findMany({
+                    where: { department_id: department.id },
+                    select: {
+                        income: true,
+                        employee: true,
+                        supply_expense: true,
+                    },
+                });
+
+                // Initialize total amount for the department
+                let totalAmount = 0;
+
+                // Calculate the sum of amounts in income, employee, and supply_expense arrays for each program
+                programs.forEach(program => {
+                    const incomeTotal: any = program.income.length == 0 ? 0 : program.income.reduce((acc, item: any) => Number(acc) + Number(item.amount), 0);
+                    const employeeTotal: any = program.employee.length == 0 ? 0 : program.employee.reduce((acc, item: any) => Number(acc) + Number(item.amount), 0);
+                    const supplyExpenseTotal = program.supply_expense.length == 0 ? 0 : program.supply_expense.reduce((acc, item: any) => Number(acc) + Number(item.amount), 0);
+
+                    totalAmount += incomeTotal + employeeTotal + supplyExpenseTotal;
+                });
+
+                // Store the total amount in the department object
+                department.totalAmount = totalAmount;
+            }
+
+            return departmentArray;
+        }
     },
 
     updateCenter: async (centerId: string, name: string) => {
@@ -66,4 +100,6 @@ export default {
             },
         });
     },
+
+
 };
